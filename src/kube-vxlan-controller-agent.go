a1380b45362b365079fa063a0b60dc0b7884a86b
Anton Alferov <anton.alferov@travelping.com>
2018-03-06 22:09:10 +0100

    Add timestamp to log output

--- src/kube-vxlan-controller-agent.go
+++ src/kube-vxlan-controller-agent.go
@@ -4,20 +4,25 @@ import (
     "fmt"
     "os"
     "os/signal"
+    "time"
     "syscall"
 )
 
 var version string
 var git_sha string
 
+const VersionFormat = "Version %s (git-%s)\n"
+const TsFormat = "2006-01-02T15:04:05.999Z"
+
 func main() {
     if len(os.Args) > 1 && os.Args[1] == "version" {
-        fmt.Printf("Version %s (git-%s)\n", version, git_sha)
+        fmt.Printf(VersionFormat, version, git_sha)
         os.Exit(0)
     }
 
     c := make(chan os.Signal, 1)
     signal.Notify(c, os.Interrupt, syscall.SIGTERM)
     <-c
-    fmt.Println("Done.")
+
+    fmt.Printf("I%s: Done.\n", time.Now().UTC().Format(TsFormat))
 }
